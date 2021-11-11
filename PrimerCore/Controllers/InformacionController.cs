0298b57fe71d363feb75709e6eae939db68727f0
nachocerrato <nacho.cr.94@gmail.com>
2021-11-11 19:39:06 +0100

    commit

---
+++ PrimerCore/Controllers/InformacionController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrimerCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrimerCore.Controllers
+{
+    public class InformacionController : Controller
+    {
+        public IActionResult Index()
+        {
+            //aquí enviamos la información
+            return View();
+        }
+
+        public IActionResult InformacionControllerView()
+        {
+            ViewBag.Nombre = "Alumno";
+            ViewBag.Apellido = "Azure";
+            ViewBag.Descripcion = "Estoy aprendiendo Net Core";
+            ViewBag.Year = 2021;
+            return View();
+        }
+
+        public IActionResult InformacionControllerViewModel()
+        {
+            List<Persona> personas = new List<Persona>();
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Persona person = new Persona();
+                person.Nombre = "Persona " + i;
+                person.Edad = 20 + i;
+                person.Email = "persona" + i + "@gmail.com";
+                personas.Add(person);
+            }
+
+            return View(personas);
+        }
+
+        public IActionResult InformacionGetViewController(String nombre, int edad)
+        {
+            ViewBag.Nombre = nombre;
+            ViewBag.Edad = edad;
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult InformacionPostViewController()
+        {
+            return View();
+        }
+
+        //tenemos que crear un método con el mismo nombre que la vista
+        //pero con la decoración [HttpPost]
+        [HttpPost]
+        public IActionResult InformacionPostViewController(String cajanombre, int cajaedad)
+        {
+            ViewBag.Nombre = cajanombre;
+            ViewBag.Edad = cajaedad;
+            return View();
+        }
+    }
+}
---
+++ PrimerCore/Controllers/PracticasController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrimerCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrimerCore.Controllers
+{
+    public class PracticasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult MenuPracticasView()
+        {
+            return View();
+        }
+
+        public IActionResult EjemploColeccionesPracticasView()
+        {
+            List<Comic> comics = new List<Comic>();
+
+            Comic comic = new Comic();
+            comic.Nombre = "Wolverine";
+            comic.Descripcion = "Hombre wolvo";
+            comic.Imagen = "https://images6.alphacoders.com/873/873021.jpg";
+            comics.Add(comic);
+
+            comic = new Comic();
+            comic.Nombre = "Spiderman";
+            comic.Descripcion = "Hombre aragna";
+            comic.Imagen = "https://www.desktopbackground.org/p/2015/11/12/1041188_comics-spider-man-marvel-wallpapers-for-high-resolution-hd-16-9_1920x1200_h.jpg";
+
+            comics.Add(comic);
+
+            comic = new Comic();
+            comic.Nombre = "Ironman";
+            comic.Descripcion = "Hombre errático";
+            comic.Imagen = "https://wallpapersmug.com/download/1600x900/ad84c3/iron-man-comicart-4k.jpg";
+
+            comics.Add(comic);
+
+            return View(comics);
+        }
+    }
+}
