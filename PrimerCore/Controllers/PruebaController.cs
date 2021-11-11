a20ab90219cfde3bc16ba3b2a4ea9d2709cfaa31
nachocerrato <nacho.cr.94@gmail.com>
2021-11-11 12:40:34 +0100

    commit

---
+++ PrimerCore/Controllers/PruebaController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrimerCore.Controllers
+{
+    public class PruebaController : Controller
+    {
+        //estamos indicando que tendremos una vista llamada Index dentro de Views/Prueba
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
